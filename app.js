5e25c8c84f5f7bef729519399c17440cd5843b83
Tanay Van <tanayvan@Tanays-MacBook-Pro.local>
2023-12-08 21:13:50 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const dotenv = require('dotenv')
+dotenv.config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const axios = require('axios');
+
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json())
+
+
+
+
+
+
+app.get('/', async (req, res) => {
+
+
+
+
+
+    res.json({
+        message: "It is working"
+    })
+})
+
+app.get('/quote', async (req, res) => {
+
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'https://api.quotable.io/random',
+        headers: {}
+    };
+
+    let response = await axios.request(config)
+
+    res.json(response.data)
+})
+
+
+const PORT = 9000;
+
+
+app.listen(PORT, () => {
+    console.log(`Server has started on ${PORT}`)
+})
